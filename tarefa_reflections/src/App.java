018bf11904f4af1ce65ec6c87da0e8a6febf7bf9
k-lila <krishnalilapalandi@gmail.com>
2025-04-16 12:34:28 -0300

    tarefa módulo 19

--- tarefa_reflections/src/App.java
+++ tarefa_reflections/src/App.java
@@ -1,13 +1,12 @@
-
-import java.lang.annotation.Annotation;
-
 import annotations.ClasseExemplo;
 import annotations.Tabela;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ClasseExemplo exemplo = new ClasseExemplo();
-        String nome = exemplo.getClass().getAnnotation(Tabela.class).nomeDaTabela();
-        System.out.println(nome);
+        // ClasseExemplo exemplo = new ClasseExemplo();
+        // String nomeTabela = exemplo.getClass().getAnnotation(Tabela.class).nomeDaTabela();
+        Class<ClasseExemplo> exemplo = ClasseExemplo.class;
+        String nomeTabela = exemplo.getAnnotation(Tabela.class).nomeDaTabela();
+        System.out.println(nomeTabela);
     }
 }
